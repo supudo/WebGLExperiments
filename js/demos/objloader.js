b273df5e8d2a3761d41becd2707be0e4b438c4cf
Sergey Petrov <supudo@gmail.com>
2015-11-06 19:08:47 +0200

    rev

--- js/demos/objloader.js
+++ js/demos/objloader.js
@@ -173,6 +173,8 @@ function OBJLoader(gl, gameCanvas) {
 
     gl.uniformMatrix4fv(matrixLocation, false, matrix);
 
+    //mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.01, 1000.0, identity());
+
     gl.drawArrays(gl.TRIANGLES, 0, model.verts.length / 3);
   };
 
@@ -191,6 +193,58 @@ function OBJLoader(gl, gameCanvas) {
     }
     textureImages.density = density;
     return textureImages;
+  };
+
+  this.getProgramInfo = function(program) {
+    var result = {
+      attributes: [],
+      uniforms: [],
+      attributeCount: 0,
+      uniformCount: 0
+    },
+    activeUniforms = gl.getProgramParameter(program, gl.ACTIVE_UNIFORMS),
+    activeAttributes = gl.getProgramParameter(program, gl.ACTIVE_ATTRIBUTES);
+
+    var enums = {
+      0x8B50: 'FLOAT_VEC2',
+      0x8B51: 'FLOAT_VEC3',
+      0x8B52: 'FLOAT_VEC4',
+      0x8B53: 'INT_VEC2',
+      0x8B54: 'INT_VEC3',
+      0x8B55: 'INT_VEC4',
+      0x8B56: 'BOOL',
+      0x8B57: 'BOOL_VEC2',
+      0x8B58: 'BOOL_VEC3',
+      0x8B59: 'BOOL_VEC4',
+      0x8B5A: 'FLOAT_MAT2',
+      0x8B5B: 'FLOAT_MAT3',
+      0x8B5C: 'FLOAT_MAT4',
+      0x8B5E: 'SAMPLER_2D',
+      0x8B60: 'SAMPLER_CUBE',
+      0x1400: 'BYTE',
+      0x1401: 'UNSIGNED_BYTE',
+      0x1402: 'SHORT',
+      0x1403: 'UNSIGNED_SHORT',
+      0x1404: 'INT',
+      0x1405: 'UNSIGNED_INT',
+      0x1406: 'FLOAT'
+    };
+
+    for (var i=0; i<activeUniforms; i++) {
+      var uniform = gl.getActiveUniform(program, i);
+      uniform.typeName = enums[uniform.type];
+      result.uniforms.push(uniform);
+      result.uniformCount += uniform.size;
+    }
+
+    for (var i=0; i<activeAttributes; i++) {
+      var attribute = gl.getActiveAttrib(program, i);
+      attribute.typeName = enums[attribute.type];
+      result.attributes.push(attribute);
+      result.attributeCount += attribute.size;
+    }
+
+    return result;
   };
 
   //
